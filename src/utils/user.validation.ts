802b95d82111f4713fc665c6c5a45ce97dbd5960
shivanisharma2022 <sharmashivani0619@gmail.com>
2022-05-02 11:35:01 +0530

    updation in user validation

--- src/utils/user.validation.ts
+++ src/utils/user.validation.ts
@@ -5,7 +5,7 @@ class userValidationClass {
 userSignup = Joi.object({
     firstName: Joi.string().trim().min(3).max(15).lowercase().required(),
     lastName: Joi.string().trim().min(3).max(15).lowercase().required(),
-    phoneNumber: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
+    phoneNumber: Joi.string().length(12).pattern(/^[0-9]+$/).required(),
     email: Joi.string().trim().email().required(),
     userType: Joi.string().trim().required()
 
